c62e97ac0c4fb4b8e11fdc894067b738b4d7e63c
Jare20 <121035959+Jare20@users.noreply.github.com>
2023-09-19 18:20:52 -0300

    Update cadastrar.js

--- js/cadastrar.js
+++ js/cadastrar.js
@@ -13,24 +13,24 @@ function cadastrar() {
     const email = document.querySelector('input[name="email"]').value;
     const password = document.querySelector('input[name="password"]').value;
   
-    // Valide o email e a senha conforme necessário (por exemplo, verificando se o email é válido e se a senha atende aos critérios)
+
   
     if (!email || !password) {
-      // Exiba uma mensagem de erro se algum dos campos estiver em branco
+     
       const msgError = document.getElementById("msgError");
       msgError.textContent = "Preencha todos os campos antes de cadastrar.";
       msgError.style.display = "block";
-      return; // Saia da função para evitar o cadastro com campos em branco
+      return; 
     }
 
     if (!email.includes("@")) {
         const msgError = document.getElementById("msgError");
         msgError.textContent = "O email inserido é inválido. Por favor, insira um email válido.";
         msgError.style.display = "block";
-        return; // Saia da função
+        return; 
       }
   
-    // Verifique se o email já está cadastrado (você pode fazer isso comparando com os registros existentes no localStorage)
+  
     const listaUsuarios = JSON.parse(localStorage.getItem("listaUsuarios")) || [];
   
     const usuarioExistente = listaUsuarios.find((usuario) => usuario.email === email);
@@ -38,24 +38,21 @@ function cadastrar() {
       const msgError = document.getElementById("msgError");
       msgError.textContent = "E-mail já cadastrado. Por favor, faça login ou use outro e-mail.";
       msgError.style.display = "block";
-      return; // Saia da função para evitar o cadastro duplicado
+      return; 
     }
   
-    // Se tudo estiver válido, crie um novo objeto de usuário
+
     const novoUsuario = {
       email,
       senha: password,
     };
 
-    // Adicione o novo usuário à lista de usuários
     listaUsuarios.push(novoUsuario);
 
-    // Salve a lista atualizada no localStorage
     localStorage.setItem("listaUsuarios", JSON.stringify(listaUsuarios));
   
-    // Limpe a mensagem de erro, se houver
     const msgError = document.getElementById("msgError");
     msgError.style.display = "none";
-    // Redirecione o usuário para a tela de login
+
     window.location.href = "../news/login.html";
   }
